63a252a6201201064b6ddcc0c0f85f74a0978656
vgguedes <vgguedes@gmail.com>
2020-04-18 18:27:28 -0300

    firebase configurado

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 // Webpack CSS import
 import VueOnsen from 'vue-onsenui'; // This imports 'onsenui', so no need to import it separately
 import 'onsenui/css/onsenui.css';
@@ -14,5 +15,18 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyArRl2HUZe73-3cFoEereimC3eNiDaDuSw",
+      authDomain: "pep-vue.firebaseapp.com",
+      databaseURL: "https://pep-vue.firebaseio.com",
+      projectId: "pep-vue",
+      storageBucket: "pep-vue.appspot.com",
+      messagingSenderId: "888940090859",
+      appId: "1:888940090859:web:7e6b8953ef4df56f83b5ae",
+      measurementId: "G-RJ9VYXGW96"
+
+    })
+  }
 }).$mount('#app')
