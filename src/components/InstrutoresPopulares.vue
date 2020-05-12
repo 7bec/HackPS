2ce923a893d1f5371065bfb57518bb128b72a030
Vinicius Guedes <vgg@cin.ufpe.br>
2020-05-12 19:09:14 -0300

    fix: identation

--- src/components/InstrutoresPopulares.vue
+++ src/components/InstrutoresPopulares.vue
@@ -5,8 +5,8 @@
     div(style='margin-top: 3vh; overflow-x: auto; white-space: nowrap !important; display: flex;')
         v-col(cols='5' v-for='(i, instrutor) in instrutoresPopulares' :key='i')
             v-row( justify='center')
-                    v-avatar(size='100')
-                        img(src='https://images.pexels.com/photos/2803158/pexels-photo-2803158.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
+                v-avatar(size='100')
+                    img(src='https://images.pexels.com/photos/2803158/pexels-photo-2803158.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
             v-row(justify='center')
                 v-container
                     span.name.font.text-xs-center Cláudio Castro
