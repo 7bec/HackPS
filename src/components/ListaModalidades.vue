c9cf462ac1db0e235dbd89810546bc962b1e4546
gui55555pl <guilherme@beyondcloud.com.br>
2020-05-13 23:03:06 -0300

    feat: paddings bottoms lista instrutores

--- src/components/ListaModalidades.vue
+++ src/components/ListaModalidades.vue
@@ -1,14 +1,15 @@
 <template lang='pug'>
-  v-container(fill-height)
-    v-row( align='center' justify='center')
-        v-col(cols='12' v-for='modalidade in modalidades' :key='modalidade.name' align-self='center' justify='center')
-            v-row( justify='center' style='margin-bottom: -10px;')
-                v-card.mx-10.rounded-card(max-width="99vw" elevation='0' @click='goTo(modalidade.name)')
-                    v-img(:src="modalidade.background_url")
-                        v-row(style='width: 100%;')
-                            div.overlay
-                            v-row(style='margin-top: 15px; margin-left: 20px;')
-                                span.titulo-card {{modalidade.name}}
+  v-container.padding0(fill-height)
+    v-row.padding0(no-gutters align='center' justify='center')
+        v-col.padding0(cols='12' v-for='modalidade in modalidades' :key='modalidade.name' align-self='center' justify='center')
+            v-row.padding0(no-gutters style='justify-content: center;')
+                v-col(cols='12')
+                    v-card.mx-10.rounded-card(max-width="99vw" elevation='0' @click='goTo(modalidade.name)' style='width: 100%; height: 35vh')
+                        div(:style='`background-image: url(${modalidade.background_url}); height: 100%;`')
+                            div(style='width: 100%; height: 100%;')
+                                div.overlay
+                                v-row(style='margin-top: 15px; margin-left: 20px; height: 100%;')
+                                    span.titulo-card {{modalidade.name}}
 </template>
 
 <script>
@@ -83,5 +84,8 @@ export default {
     background: linear-gradient(to bottom, rgba(19,19,19,1) 0%, rgba(28,28,28,0.64) 9%, rgba(76,76,76,0.2) 20%, rgba(43,43,43,0.2) 24%, rgba(17,17,17,0.2) 40%, rgba(0,0,0,0.2) 49%, rgba(44,44,44,0.2) 50%, rgba(71,71,71,0.2) 61%, rgba(102,102,102,0.2) 75%, rgba(89,89,89,0.2) 88%);
     filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#131313', endColorstr='#595959', GradientType=0 );    
     color:black;
+}
+.padding0 {
+    padding: 0;
 }
 </style>
